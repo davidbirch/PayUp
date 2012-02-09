e2954ce3e5d32ee5d0043e659ac90bd8e0885df8
David Birch <birch.dave@gmail.com>
2012-02-09 23:35:38 +1100

    Added default store page

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,2 +1,19 @@
 class Product < ActiveRecord::Base
+  
+  # validate these mandatory fields
+  validates :title, :description, :image_url, presence: true
+  
+  # validate the price is a positive value
+  validates :price, numericality: {greater_than_or_equal_to: 0.01}
+  
+  # validate a unique title
+  validates :title, uniqueness: true
+  
+  # validate the image refers to a url
+  validates :image_url, allow_blank: true, format: {
+  	with: %r{\.(gif|jpg|png)$}i,
+  	message: 'must be a URL for GIF, JPG or PNG image.'
+  	}
+  
+  
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -2,38 +2,22 @@
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 
 Product.delete_all
-Product.create(title: 'CoffeeScript',
-  description: 
-    %{<p>
-        CoffeeScript is JavaScript done right. It provides all of JavaScript's
-	functionality wrapped in a cleaner, more succinct syntax. In the first
-	book on this exciting new language, CoffeeScript guru Trevor Burnham
-	shows you how to hold onto all the power and flexibility of JavaScript
-	while writing clearer, cleaner, and safer code.
-      </p>},
-  image_url:   'cs.jpg',    
-  price: 36.00)
+Product.create(title: 'One dozen Apples',
+  description: 'A dozen of the finest apples.',
+  image_url:   'apple.jpg',    
+  price: 5.00)
 # . . .
-Product.create(title: 'Programming Ruby 1.9',
-  description:
-    %{<p>
-        Ruby is the fastest growing and most exciting dynamic language
-        out there. If you need to get working programs delivered fast,
-        you should add Ruby to your toolbox.
-      </p>},
-  image_url: 'ruby.jpg',
-  price: 49.95)
+Product.create(title: 'Bag of Oranges',
+  description:'A bag of tasty oranges.',
+  image_url: 'orange.jpg',
+  price: 20.00)
 # . . .
-
-Product.create(title: 'Rails Test Prescriptions',
-  description: 
-    %{<p>
-        <em>Rails Test Prescriptions</em> is a comprehensive guide to testing
-        Rails applications, covering Test-Driven Development from both a
-        theoretical perspective (why to test) and from a practical perspective
-        (how to test effectively). It covers the core Rails testing tools and
-        procedures for Rails 2 and Rails 3, and introduces popular add-ons,
-        including Cucumber, Shoulda, Machinist, Mocha, and Rcov.
-      </p>},
-  image_url: 'rtp.jpg',
-  price: 34.95)
+Product.create(title: 'Bunch of Bananas',
+  description:'<em>Far North Queensland</em> quality bananas.',
+  image_url: 'banana.jpg',
+  price: 15.00)
+# . . .
+Product.create(title: 'Lemon',
+  description:'A lemon',
+  image_url: 'lemon.jpg',
+  price: 2.00)
